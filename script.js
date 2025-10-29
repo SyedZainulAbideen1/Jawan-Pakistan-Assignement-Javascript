4ed2afe0d12c32c0ddb2ee306084d44e70b172cd
SyedZainulAbideen1 <zain1234567q@gmail.com>
2025-10-29 21:07:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Head section alert
+console.log("Script inside HEAD is running!");
+
+alert("Welcome to my website!"); // Q1
+
+alert("Error! Please enter a valid password."); // Q2
+
+alert("Welcome to JS Land...\nHappy Coding!"); // Q3
+
+alert("Welcome to JS Land...");  // Q4 (sequence 1)
+alert("Happy Coding!");          // Q4 (sequence 2)
+
+console.log("Hello... I can run JS through my web browser's console"); // Q5
+
+console.log("Script after BODY is running!");
